d36cab1d1b0a30be07ae8fac7985c91a8f463fa7
Evgeniy Gazetdinov <zplacesound@gmail.com>
2021-06-30 05:12:30 +0300

    add count time in terminal

--- timer_main.py
+++ timer_main.py
@@ -2,6 +2,8 @@ from threading import Timer,Thread,Event
 import os
 import sys 
 import subprocess
+import time
+import datetime 
 
 class perpetualTimer():
 
@@ -12,10 +14,21 @@ class perpetualTimer():
 
    def handle_function(self):
       self.hFunction()
+
       self.thread = Timer(self.t,self.handle_function)
       self.thread.start()
 
+
+   def count_time(self):
+      counter = self.t
+      while(counter):
+        counter-=1
+        time.sleep(1)
+        print(str(datetime.timedelta(seconds=counter)))
+      print('stop timer') 
+
    def start(self):
+      self.count_time()
       self.thread.start()
 
    def cancel(self):
